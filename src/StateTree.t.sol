ed85a3c97fda78641792bef60fdae8eb935bf5eb
Tim Daubenschütz <tim@daubenschuetz.de>
2021-12-17 19:43:33 +0100

    sorted hashes

--- src/StateTree.t.sol
+++ src/StateTree.t.sol
@@ -1,4 +1,4 @@
-// @format
+// SPDX-License-Identifier: GPL-3.0-only
 pragma solidity ^0.8.6;
 
 import "ds-test/test.sol";
@@ -7,199 +7,111 @@ import "./StateTree.sol";
 contract StateTreeTest is DSTest {
     function setUp() public {}
 
-    function testGasOfGet() public {
-        uint startGas = gasleft();
-        StateTree.get(0);
-        uint endGas = gasleft();
-        emit log_named_uint("gas calling get(uint256 _level)", startGas - endGas);
-    }
-
     function testGasOfCompute() public {
-		bytes32 LEAF = 0x0000000000000000000000000000000000000000000000000000000000000000;
-		bytes32 LEAF_HASH = keccak256(abi.encode(LEAF));
-        bytes32 emptyTree = StateTree.empty();
-		bytes32[] memory proofs = new bytes32[](0);
+		bytes32[] memory proofs = new bytes32[](8);
 
         uint startGas = gasleft();
-	    StateTree.validate(proofs, 0, 0, LEAF_HASH, emptyTree);
+	    bytes32 root = StateTree.compute(proofs);
         uint endGas = gasleft();
 
+        assertEq(root, 0);
         emit log_named_uint("gas calling compute()", startGas - endGas);
     }
 
-    function testBitwiseProofBitGeneration() public {
-        // eval pos 0
-        uint8 value = StateTree.bitmap(0);
-        value += StateTree.bitmap(4);
-        assertEq(value % 2, 1);
-
-        // eval pos 1
-        value = value / 2;
-        assertEq(value % 2, 0);
-
-        // eval pos 2
-        value = value / 2;
-        assertEq(value % 2, 0);
-
-        // eval pos 3
-        value = value / 2;
-        assertEq(value % 2, 0);
-
-        // eval pos 4
-        value = value / 2;
-        assertEq(value % 2, 1);
-
-        // eval pos 5
-        value = value / 2;
-        assertEq(value % 2, 0);
-
-        // eval pos 6
-        value = value / 2;
-        assertEq(value % 2, 0);
-
-        // eval pos 7
-        value = value / 2;
-        assertEq(value % 2, 0);
-    }
-
-    function testGetHash() public {
-        bytes32 hash = StateTree.get(0);
-		assertEq(hash, 0x290decd9548b62a8d60345a988386fc84ba6bc95484008f6362f93160ef3e563);
-    }
 
     function testGettingZero() public {
         bytes32 empty = StateTree.empty();
-		assertEq(empty, 0xe1cea92ed99acdcb045a6726b2f87107e8a61620a232cf4d7d5b5766b3952e10);
+		assertEq(empty, 0);
     }
 
-	function testComputeEmpty() public {
-		bytes32[] memory proofs = new bytes32[](0);
-
-		bytes32 LEAF = 0x0000000000000000000000000000000000000000000000000000000000000000;
-		bytes32 LEAF_HASH = keccak256(abi.encode(LEAF));
-
-		bytes32 expectedRoot = StateTree.empty();
-		assertEq(StateTree.compute(proofs, 0, 0, LEAF_HASH), expectedRoot);
-	}
-
 	function testValidateEmpty() public {
-		bytes32[] memory proofs = new bytes32[](0);
-
-		bytes32 LEAF = 0x0000000000000000000000000000000000000000000000000000000000000000;
-		bytes32 LEAF_HASH = keccak256(abi.encode(LEAF));
+		bytes32[] memory proofs = new bytes32[](8);
 
 		bytes32 expectedRoot = StateTree.empty();
-		assertTrue(
-			StateTree.validate(proofs, 0, 0, LEAF_HASH,
-			expectedRoot
-		));
+		assertTrue(StateTree.validate(proofs, expectedRoot));
 	}
 
 	function testComputeInsertFirst() public {
-		bytes32[] memory proofs = new bytes32[](0);
+		bytes32[] memory proofs = new bytes32[](8);
 
 		bytes32 LEAF = 0x0000000000000000000000000000000000000000000000000000000000000001;
 		bytes32 LEAF_HASH = keccak256(abi.encode(LEAF));
+        proofs[0] = LEAF_HASH;
 
 		bytes32 expectedRoot = LEAF_HASH;
-        uint DEPTH = 8;
-     	for (uint256 i = 0; i < DEPTH; i++) {
-			expectedRoot = keccak256(abi.encode(expectedRoot, StateTree.get(i)));
+     	for (uint256 i = 1; i < DEPTH; i++) {
+			expectedRoot = keccak256(abi.encode(expectedRoot, 0));
 		}
 
-		assertEq(StateTree.compute(proofs, 0, 0, LEAF_HASH), expectedRoot);
+		assertEq(StateTree.compute(proofs), expectedRoot);
 	}
 
 	function testWriteFirst() public {
-		bytes32[] memory proofs = new bytes32[](0);
+		bytes32[] memory proofs = new bytes32[](8);
+		bytes32[] memory overwrite = new bytes32[](2);
 
 		bytes32 NEXT_LEAF = 0x0000000000000000000000000000000000000000000000000000000000000001;
 		bytes32 NEXT_LEAF_HASH = keccak256(abi.encode(NEXT_LEAF));
-
-		bytes32 PREV_LEAF = 0x0000000000000000000000000000000000000000000000000000000000000000;
-		bytes32 PREV_LEAF_HASH = keccak256(abi.encode(PREV_LEAF));
+        overwrite[0] = NEXT_LEAF_HASH;
 
 		bytes32 PREV_ROOT = StateTree.empty();
         uint startGas = gasleft();
-		bytes32 NEXT_ROOT = StateTree.write(proofs, 0, 0, NEXT_LEAF_HASH, PREV_LEAF_HASH, PREV_ROOT);
+		bytes32 NEXT_ROOT = StateTree.write(proofs, overwrite, PREV_ROOT);
         uint endGas = gasleft();
-        emit log_named_uint("gas calling first write()", startGas - endGas);
+        emit log_named_uint("gas calling write()", startGas - endGas);
 
 		bytes32 expectedRoot = NEXT_LEAF_HASH;
-        uint DEPTH = 8;
-     	for (uint256 i = 0; i < DEPTH; i++) {
-			expectedRoot = keccak256(abi.encode(expectedRoot, StateTree.get(i)));
+     	for (uint256 i = 1; i < DEPTH; i++) {
+			expectedRoot = keccak256(abi.encode(expectedRoot, 0));
 		}
 		assertEq(NEXT_ROOT, expectedRoot);
 	}
 
-	function testWriteTwo() public {
-		bytes32[] memory proofs = new bytes32[](0);
+	function testWriteThree() public {
+		bytes32[] memory proofs = new bytes32[](8);
+		bytes32[] memory overwrite = new bytes32[](2);
 
 		bytes32 NEXT_LEAF = 0x0000000000000000000000000000000000000000000000000000000000000001;
 		bytes32 NEXT_LEAF_HASH = keccak256(abi.encode(NEXT_LEAF));
-
-		bytes32 PREV_LEAF = 0x0000000000000000000000000000000000000000000000000000000000000000;
-		bytes32 PREV_LEAF_HASH = keccak256(abi.encode(PREV_LEAF));
+        overwrite[0] = NEXT_LEAF_HASH;
 
 		bytes32 ROOT1 = StateTree.empty();
-		bytes32 ROOT2 = StateTree.write(proofs, 0, 0, NEXT_LEAF_HASH, PREV_LEAF_HASH, ROOT1);
+		bytes32 ROOT2 = StateTree.write(proofs, overwrite, ROOT1);
 
-        uint8 bits = StateTree.bitmap(0);
-		bytes32[] memory proofs1 = new bytes32[](1);
-        proofs1[0] = NEXT_LEAF_HASH;
-		bytes32 ROOT3 = StateTree.write(proofs1, bits, 1, NEXT_LEAF_HASH, PREV_LEAF_HASH, ROOT2);
-	}
+        bytes32 expectedRoot = NEXT_LEAF_HASH;
+     	for (uint256 i = 1; i < DEPTH; i++) {
+			expectedRoot = keccak256(abi.encode(expectedRoot, 0));
+		}
+		assertEq(ROOT2, expectedRoot);
 
-	function testFillUpTree() public {
-		uint256 DEPTH = 8;
-	    bytes32 LEAF = 0x0000000000000000000000000000000000000000000000000000000000000001;
-		bytes32 LEAF_HASH = keccak256(abi.encode(LEAF));
-		bytes32 ZERO_LEAF = 0x0000000000000000000000000000000000000000000000000000000000000000;
-		bytes32 ZERO_LEAF_HASH = keccak256(abi.encode(ZERO_LEAF));
+		bytes32[] memory proofs2 = new bytes32[](8);
+        proofs2[0] = NEXT_LEAF_HASH;
+		bytes32[] memory overwrite2 = new bytes32[](2);
+        overwrite2[0] = NEXT_LEAF_HASH;
+        overwrite2[1] = NEXT_LEAF_HASH;
+		bytes32 ROOT3 = StateTree.write(proofs2, overwrite2, ROOT2);
 
-        bytes32[] memory ones = new bytes32[](DEPTH);
-        bytes32 hashOnes = LEAF_HASH;
-        ones[0] = LEAF_HASH;
-        ones[1] = keccak256(abi.encode(ones[0], ones[0]));
-        ones[2] = keccak256(abi.encode(ones[1], ones[1]));
-        ones[3] = keccak256(abi.encode(ones[2], ones[2]));
-        ones[4] = keccak256(abi.encode(ones[3], ones[3]));
-        ones[5] = keccak256(abi.encode(ones[4], ones[4]));
-        ones[6] = keccak256(abi.encode(ones[5], ones[5]));
-        ones[7] = keccak256(abi.encode(ones[6], ones[6]));
-
- 		bytes32 prevRoot = StateTree.empty();
-        for(uint256 i = 0; i < (2**DEPTH)-1; i++) {
-            bytes32[] memory proofs = new bytes32[](DEPTH);
-
-			uint8 bits;
-            uint256 pointer = i;
-            for(uint8 j = 0; j < DEPTH; j++) {
-                if(pointer % 2 == 0) {
-                    //proofs[j] = zeros[j];
-                } else {
-                	bits += StateTree.bitmap(j);
-                    proofs[j] = ones[j];
-                }
-                pointer = pointer / 2;
-            }
-
-            prevRoot = StateTree.write(proofs, bits, i, LEAF_HASH, ZERO_LEAF_HASH, prevRoot);
+        bytes32 expectedRoot2 = keccak256(abi.encode(NEXT_LEAF_HASH, NEXT_LEAF_HASH));
+        for(uint256 i = 2; i < DEPTH; i++) {
+            expectedRoot2 = keccak256(abi.encode(expectedRoot2, 0));
         }
+		assertEq(ROOT3, expectedRoot2);
 
+		bytes32[] memory proofs3 = new bytes32[](7);
+        proofs3[0] = keccak256(abi.encode(NEXT_LEAF_HASH, NEXT_LEAF_HASH));
+		assertTrue(StateTree.validate(proofs3, ROOT3));
+
+		bytes32[] memory overwrite3 = new bytes32[](2);
+		overwrite3[0] = proofs3[0];
+        overwrite3[1] = keccak256(abi.encode(NEXT_LEAF_HASH, 0));
+		StateTree.write(proofs3, overwrite3, ROOT3);
 	}
 
+   function testValidateAll() public {
+        bytes32 LEAF = 0x0000000000000000000000000000000000000000000000000000000000000001;
+        bytes32 LEAF_HASH = keccak256(abi.encode(LEAF));
 
-	function testFillUp() public {
-		uint256 DEPTH = 8;
-	    bytes32 LEAF = 0x0000000000000000000000000000000000000000000000000000000000000001;
-		bytes32 LEAF_HASH = keccak256(abi.encode(LEAF));
-		bytes32 ZERO_LEAF = 0x0000000000000000000000000000000000000000000000000000000000000000;
-		bytes32 ZERO_LEAF_HASH = keccak256(abi.encode(ZERO_LEAF));
-
-        bytes32[] memory ones = new bytes32[](DEPTH);
-        bytes32 hashOnes = LEAF_HASH;
+		bytes32[] memory ones = new bytes32[](DEPTH);
         ones[0] = LEAF_HASH;
         ones[1] = keccak256(abi.encode(ones[0], ones[0]));
         ones[2] = keccak256(abi.encode(ones[1], ones[1]));
@@ -209,68 +121,117 @@ contract StateTreeTest is DSTest {
         ones[6] = keccak256(abi.encode(ones[5], ones[5]));
         ones[7] = keccak256(abi.encode(ones[6], ones[6]));
 
-        bytes32 prevRoot = StateTree.empty();
-        for(uint256 i = 0; i < 5; i++) {
-            bytes32[] memory proofs = new bytes32[](DEPTH);
+		bytes32 prevRoot = StateTree.empty();
+        for(uint256 i = 0; i < (2**DEPTH)-1; i++) {
+            bytes32[] memory proofs = new bytes32[](8);
 
-            uint8 bits;
-            if (i == 0) {
-                bits = 0;
-            } else if (i == 1) {
-                bits = StateTree.bitmap(0);
-                proofs[0] = ones[0];
-            } else if (i == 2) {
-                bits = StateTree.bitmap(1);
-                proofs[1] = ones[1];
-            } else if (i == 3) {
-                bits = StateTree.bitmap(0);
-                bits += StateTree.bitmap(1);
-                proofs[0] = ones[0];
-                proofs[1] = ones[1];
-            } else if (i == 4) {
-                bits = StateTree.bitmap(2);
-                proofs[2] = ones[2];
+			uint256 p = i;
+			uint256 d = 0;
+			while(p != 0) {
+				if (p % 2 != 0) {
+					proofs[d] = ones[d];
+				}
+
+				p /= 2;
+				d += 1;
 			}
 
-		    prevRoot = StateTree.write(proofs, bits, i, LEAF_HASH, ZERO_LEAF_HASH, prevRoot);
+            StateTree.validate(proofs, prevRoot);
+			bytes32 newRoot = StateTree.compute(proofs);
+			prevRoot = newRoot;
         }
-	}
-
-    function testFailHijackingHash() public {
-		bytes32[] memory proofs = new bytes32[](0);
-        uint8 bits = StateTree.bitmap(0);
-
-	    bytes32 LEAF = 0x0000000000000000000000000000000000000000000000000000000000001337;
-		bytes32 LEAF_HASH = keccak256(abi.encode(LEAF));
-		bytes32 ZERO_LEAF = 0x0000000000000000000000000000000000000000000000000000000000000000;
-		bytes32 ZERO_LEAF_HASH = keccak256(abi.encode(ZERO_LEAF));
-
-	    bytes32 newRoot = StateTree.write(proofs, bits, 0, LEAF_HASH, ZERO_LEAF_HASH, ZERO_LEAF_HASH);
-        assertEq(newRoot, LEAF_HASH);
     }
 
-	function testUpdatingFirstEntryAfterAdditionalWrite() public {
-		bytes32[] memory proofs = new bytes32[](0);
+   function testWriteAll() public {
+        bytes32 LEAF = 0x0000000000000000000000000000000000000000000000000000000000000001;
+        bytes32 LEAF_HASH = keccak256(abi.encode(LEAF));
 
-		bytes32 NEXT_LEAF = 0x0000000000000000000000000000000000000000000000000000000000000001;
-		bytes32 NEXT_LEAF_HASH = keccak256(abi.encode(NEXT_LEAF));
+		bytes32[] memory ones = new bytes32[](DEPTH);
+        ones[0] = LEAF_HASH;
+        ones[1] = keccak256(abi.encode(ones[0], ones[0]));
+        ones[2] = keccak256(abi.encode(ones[1], ones[1]));
+        ones[3] = keccak256(abi.encode(ones[2], ones[2]));
+        ones[4] = keccak256(abi.encode(ones[3], ones[3]));
+        ones[5] = keccak256(abi.encode(ones[4], ones[4]));
+        ones[6] = keccak256(abi.encode(ones[5], ones[5]));
+        ones[7] = keccak256(abi.encode(ones[6], ones[6]));
 
-		bytes32 PREV_LEAF = 0x0000000000000000000000000000000000000000000000000000000000000000;
-		bytes32 PREV_LEAF_HASH = keccak256(abi.encode(PREV_LEAF));
+		bytes32 prevRoot = StateTree.empty();
+        //for(uint256 i = 0; i < (2**DEPTH)-1; i++) {
+        for(uint256 i = 0; i < 4; i++) {
+            bytes32[] memory proofs = new bytes32[](8);
+            bytes32[] memory overwrite = new bytes32[](2);
 
-		bytes32 ROOT1 = StateTree.empty();
-		bytes32 ROOT2 = StateTree.write(proofs, 0, 0, NEXT_LEAF_HASH, PREV_LEAF_HASH, ROOT1);
+			uint256 p = i;
+			uint256 d = 0;
+			while(p != 0) {
+				if (p % 2 == 1) {
+					proofs[d] = ones[d];
+				}
 
-        uint8 bits = StateTree.bitmap(0);
-		bytes32[] memory proofs1 = new bytes32[](1);
-        proofs1[0] = NEXT_LEAF_HASH;
-		bytes32 ROOT3 = StateTree.write(proofs1, bits, 1, NEXT_LEAF_HASH, PREV_LEAF_HASH, ROOT2);
+				p /= 2;
+				d += 1;
+			}
 
-		bytes32 UPDATE_LEAF = 0x0000000000000000000000000000000000000000000000000000000000000002;
-		bytes32 UPDATE_LEAF_HASH = keccak256(abi.encode(UPDATE_LEAF));
-        uint8 bits2 = StateTree.bitmap(0);
-		bytes32[] memory proofs2 = new bytes32[](1);
-        proofs2[0] = NEXT_LEAF_HASH;
-		bytes32 ROOT4 = StateTree.write(proofs2, bits2, 0, UPDATE_LEAF_HASH, proofs2[0], ROOT3);
-	}
+			if (i == 0) {
+				assertEq(proofs[0], 0);
+				assertEq(proofs[1], 0);
+				overwrite[0] = LEAF_HASH;
+			} else if (i == 1) {
+				assertEq(proofs[0], LEAF_HASH);
+				assertEq(proofs[1], 0);
+				overwrite[0] = proofs[0];
+				overwrite[1] = LEAF_HASH;
+			} else if (i == 2) {
+				assertEq(prevRoot, 0x7605c12dd7ba63a76312ebe20bcab106e02e3ef3db29bb11167883778be84915); 
+				assertEq(proofs[0], 0);
+				assertEq(proofs[1], ones[1]);
+				overwrite[0] = LEAF_HASH;
+				overwrite[1] = 0;
+			} else if (i == 3) {
+				overwrite[0] = LEAF_HASH;
+				overwrite[1] = LEAF_HASH;
+			}
+
+			prevRoot = StateTree.write(proofs, overwrite, prevRoot);
+        }
+    }
+
+    //function testFailHijackingHash() public {
+	//	bytes32[] memory proofs = new bytes32[](0);
+    //    uint8 bits = StateTree.bitmap(0);
+
+	//    bytes32 LEAF = 0x0000000000000000000000000000000000000000000000000000000000001337;
+	//	bytes32 LEAF_HASH = keccak256(abi.encode(LEAF));
+	//	bytes32 ZERO_LEAF = 0x0000000000000000000000000000000000000000000000000000000000000000;
+	//	bytes32 ZERO_LEAF_HASH = keccak256(abi.encode(ZERO_LEAF));
+
+	//    bytes32 newRoot = StateTree.write(proofs, bits, 0, LEAF_HASH, ZERO_LEAF_HASH, ZERO_LEAF_HASH);
+    //    assertEq(newRoot, LEAF_HASH);
+    //}
+
+	//function testUpdatingFirstEntryAfterAdditionalWrite() public {
+	//	bytes32[] memory proofs = new bytes32[](0);
+
+	//	bytes32 NEXT_LEAF = 0x0000000000000000000000000000000000000000000000000000000000000001;
+	//	bytes32 NEXT_LEAF_HASH = keccak256(abi.encode(NEXT_LEAF));
+
+	//	bytes32 PREV_LEAF = 0x0000000000000000000000000000000000000000000000000000000000000000;
+	//	bytes32 PREV_LEAF_HASH = keccak256(abi.encode(PREV_LEAF));
+
+	//	bytes32 ROOT1 = StateTree.empty();
+	//	bytes32 ROOT2 = StateTree.write(proofs, 0, 0, NEXT_LEAF_HASH, PREV_LEAF_HASH, ROOT1);
+
+    //    uint8 bits = StateTree.bitmap(0);
+	//	bytes32[] memory proofs1 = new bytes32[](1);
+    //    proofs1[0] = NEXT_LEAF_HASH;
+	//	bytes32 ROOT3 = StateTree.write(proofs1, bits, 1, NEXT_LEAF_HASH, PREV_LEAF_HASH, ROOT2);
+
+	//	bytes32 UPDATE_LEAF = 0x0000000000000000000000000000000000000000000000000000000000000002;
+	//	bytes32 UPDATE_LEAF_HASH = keccak256(abi.encode(UPDATE_LEAF));
+    //    uint8 bits2 = StateTree.bitmap(0);
+	//	bytes32[] memory proofs2 = new bytes32[](1);
+    //    proofs2[0] = NEXT_LEAF_HASH;
+	//	bytes32 ROOT4 = StateTree.write(proofs2, bits2, 0, UPDATE_LEAF_HASH, proofs2[0], ROOT3);
+	//}
 }
